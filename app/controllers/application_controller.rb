a6aa47621cc3be5e4a09e4a18ff79a563c17c4e9
Heather Creighton <heather.creighton@gmail.com>
2017-05-04 22:05:32 -0400

    added cart resources

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  before_filter :categories, :brands
+
+ #Fills the dropdown for our category link on the navbar
+  def categories
+  	@categories = Category.all
+  end
+
+  def brands
+  	@brands =  Product.pluck(:brand).sort.uniq
+  end	
+  	
 end
---
+++ app/models/order.rb
@@ -0,0 +1,7 @@
+class Order < ActiveRecord::Base
+
+	has_many :lineitems
+	belongs_to :user
+
+	serialize :order_items, Hash
+end
